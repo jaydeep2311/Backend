2125e37d73ab414d88f210ecbc73e0e0fddb5ed4
jaydeep2311 <100584963+jaydeep2311@users.noreply.github.com>
2022-04-15 14:40:33 +0530

    Initial commit

---
+++ divide.js
@@ -0,0 +1,4 @@
+function divide(a, b) {
+  return parseInt(a / b);
+}
+module.exports = divide;
---
+++ main.js
@@ -0,0 +1,9 @@
+const add = require("./add");
+const sub = require("./mul");
+const mul = require("./mul");
+const divide = require("./divide");
+
+console.log(add(1, 2));
+console.log(sub(2, 1));
+console.log(mul(2, 2));
+console.log(divide(5, 5));
